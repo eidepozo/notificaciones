8647b7f9c214e06f073aff399d79c584bb087727
eidepozo <eidepozo@gmail.com>
2022-01-28 15:12:38 -0300

    Primer commit

---
+++ lib/screens/message_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MessageScreen extends StatelessWidget {
+  const MessageScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Message')),
+        body: Center(
+            child: Text('MessageScreen', style: TextStyle(fontSize: 16))));
+  }
+}

ad4c819676ee79b4857e4d9e282a81e8e21adc58
eidepozo <eide@ext.grupogtd.com>
2022-02-07 23:05:46 -0300

    Agrega configuraciones para envio de notificaciones mediante FlutterFire

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,22 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void initState() {
+    super.initState();
+    PushNotificationService.messageStream.listen((message) {
+      print('MyApp: $message');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
